a9f3807cc1aa3d317b978581f1ddded359bd281f
Sebastian Werner <s.werner@sebastian-software.de>
2024-02-29 19:18:31 +0100

    feat: added basic vanillaExtract

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import { vercelPreset } from "@vercel/remix/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 
 installGlobals();
 
 export default defineConfig({
-  plugins: [remix({ presets: [vercelPreset()] }), tsconfigPaths()],
+  plugins: [
+    remix({ presets: [vercelPreset()] }),
+    tsconfigPaths(),
+    vanillaExtractPlugin(),
+  ],
 });
