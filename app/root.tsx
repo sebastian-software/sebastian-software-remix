5f25b2ca081974df30793a252e0a34763c916d04
Sebastian Werner <s.werner@sebastian-software.de>
2024-02-29 15:50:31 +0100

    feat: apply basic branding

--- app/root.tsx
+++ app/root.tsx
@@ -5,10 +5,11 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
+import { PropsWithChildren } from "react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
+export function Layout({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
