cd980c75c7cb7df4ad9ad4c0904d66fc7fc5a2b1
Sebastian Werner <s.werner@sebastian-software.de>
2024-03-07 16:11:16 +0100

    feat: initial home page content

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
+import { Link } from "@remix-run/react";
+import { RichText } from "~/components/richtext/RichText";
 
 export const meta: MetaFunction = () => {
   return [
@@ -9,9 +11,25 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <div>
+    <RichText>
       <h1>Sebastian Software</h1>
-      <p>Hello World.</p>
-    </div>
+      <h2>Consulting</h2>
+
+      <p>
+        Wir sind ein Team aus passionierten Experten, das sich auf die
+        Entwicklung langlebiger, hochwertiger React-Anwendungen spezialisiert
+        hat. Mit über 7 Jahren Fokussierung auf React bieten wir tiefgreifende
+        Lösungen, die den Kern Ihrer technischen Herausforderungen treffen.
+        Unser Ansatz kombiniert innovative Technologien mit einer klaren Vision
+        für Nutzererfahrung und Architektur, um Produkte zu schaffen, die nicht
+        nur heute, sondern auch in Zukunft begeistern. Entdecken Sie mit uns die
+        Zukunft der Softwareentwicklung – nachhaltig, zuverlässig und immer
+        einen Schritt voraus.
+      </p>
+
+      <p>
+        <Link to="/consulting">Mehr erfahren</Link>
+      </p>
+    </RichText>
   );
 }
