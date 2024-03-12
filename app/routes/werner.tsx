7884fbdfaf5da770627447c303c418e66c7c757e
Sebastian Werner <s.werner@sebastian-software.de>
2024-03-12 12:06:44 +0100

    feat: render werner.json in React

--- app/routes/werner.tsx
+++ app/routes/werner.tsx
@@ -1,10 +1,20 @@
 import { RichText } from "~/components/richtext/RichText";
 
+import data from "../data/werner.json";
+import { useLoaderData } from "@remix-run/react";
+import { ProjectList } from "~/components/project-list/ProjectList";
+
+export const loader = async () => {
+  return data;
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <RichText>
       <h1>Sebastian Werner</h1>
-      <p>TODO</p>
+      <ProjectList data={data.projects} />
     </RichText>
   );
 }
