ba5df07047963817c096dd9a249953f10f5f10c4
Sebastian Werner <s.werner@sebastian-software.de>
2024-04-05 20:55:14 +0200

    fix: cleanup

--- app/components/header/Header.css.ts
+++ app/components/header/Header.css.ts
@@ -1,11 +1,7 @@
-import { buildShades, buildSpectrum } from "@effective/color"
+import { buildSpectrum } from "@effective/color"
 import { style } from "@vanilla-extract/css"
 
 const vivaMagenta = buildSpectrum("BB2649")
-const darkViolet = buildShades("#000", "#534f60")
-const brightViolet = buildShades("#fff", "#534f60")
-
-console.log(darkViolet)
 
 export const root = style({
   background: `linear-gradient(to bottom, ${vivaMagenta["0"]}, ${vivaMagenta["-2"]} )`,
