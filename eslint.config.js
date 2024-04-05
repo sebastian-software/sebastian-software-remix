13f21b9b59ec38acd73a2404c7aca9ad3c7b6788
Sebastian Werner <s.werner@sebastian-software.de>
2024-04-05 21:31:25 +0200

    fix: anothter lint fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,23 @@
 import { recommended } from "@effective/eslint-config"
 
-export default [recommended]
+export default [
+  recommended,
+  {
+    files: ["**/*.css.ts"],
+    rules: {
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "variable",
+          format: ["camelCase"],
+          leadingUnderscore: "forbid",
+          trailingUnderscore: "forbid",
+          filter: {
+            regex: "[- ]",
+            match: false
+          }
+        }
+      ]
+    }
+  }
+]
