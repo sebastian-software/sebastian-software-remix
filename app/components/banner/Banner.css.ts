d4332d6252b44a11ac41028ff871172acecf7dbe
Sebastian Werner <s.werner@sebastian-software.de>
2024-07-19 16:42:08 +0200

    feat: visual tweaks

--- app/components/banner/Banner.css.ts
+++ app/components/banner/Banner.css.ts
@@ -4,13 +4,10 @@ import { style } from "@vanilla-extract/css"
 const darkViolet = buildSpectrum("3F2B3D")
 
 export const root = style({
-  background: darkViolet["0"],
-
-  textRendering: "optimizeLegibility",
-
+  background: `linear-gradient(to bottom, ${darkViolet["+2"]}, ${darkViolet["0"]})`,
   color: "white",
   paddingInline: "4rem",
-  paddingBlock: "6rem",
+  paddingBlock: "8rem",
   fontSize: "5rem",
   lineHeight: "1.15",
   textWrap: "balance"
--- app/components/card/Card.css.ts
+++ app/components/card/Card.css.ts
@@ -1,16 +1,19 @@
-import { buildSpectrum } from "@effective/color"
+import { buildShades, buildSpectrum } from "@effective/color"
 import { style } from "@vanilla-extract/css"
 
 import { elenaWebfont } from "../fonts/elena/Elena.css"
 
 const gold = buildSpectrum("C89F6B")
+const goldShades = buildShades("#fff", "C89F6B")
 
 export const root = style({
+  width: "40ch",
   border: "1px solid",
-  background: "white",
+  background: `radial-gradient(at right bottom, ${goldShades[3]}, white)`,
   borderColor: "white",
   padding: "2rem",
-  borderRadius: "0.5rem"
+  borderRadius: "0.5rem",
+  boxShadow: "0px 0px 0.5rem rgba(0, 0, 0, 0.2)"
 })
 
 export const title = style({
@@ -31,6 +34,7 @@ export const footer = style({
 export const container = style({
   background: `radial-gradient(at left top, ${gold["+5"]}, ${gold["0"]})`,
   display: "flex",
-  gap: "2rem",
-  padding: "4rem"
+  gap: "2.5rem",
+  paddingBlock: "4rem",
+  paddingInline: "10rem"
 })
