cafb6ec87df322f24184fed5d282d8918240bef0
Sebastian Werner <s.werner@sebastian-software.de>
2024-07-22 14:31:04 +0200

    feat: added banner image

--- app/components/banner/Banner.css.ts
+++ app/components/banner/Banner.css.ts
@@ -2,23 +2,26 @@ import { buildSpectrum } from "@effective/color"
 import { style } from "@vanilla-extract/css"
 
 import { elenaWebfont } from "../fonts/elena/Elena.css"
+import bannerImage from "./assets/566ak-gen1.jpg"
 
 const darkViolet = buildSpectrum("3F2B3D")
+console.log("XXX:", darkViolet)
 
 export const rootClass = style({
-  background: `linear-gradient(to bottom, ${darkViolet["+2"]}, ${darkViolet["0"]})`,
+  backgroundImage: `url(${bannerImage})`,
+  backgroundPosition: "center",
+  backgroundSize: "auto 100%",
+  backgroundRepeat: "no-repeat",
   color: "white",
-  paddingInline: "4rem",
-  paddingBlock: "8rem",
-  fontSize: "5rem",
-  lineHeight: "1.15",
-  textWrap: "balance"
-})
-
-export const titleClass = style({
-  fontWeight: "normal"
+  height: "35vw"
 })
 
 export const contentClass = style({
-  fontFamily: elenaWebfont
+  fontFamily: elenaWebfont,
+  fontSize: "3vw",
+  lineHeight: "1.2",
+  textWrap: "balance",
+  maxWidth: "50ch",
+  marginInline: "auto",
+  paddingTop: "24vw"
 })
--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -1,15 +1,14 @@
 import type { PropsWithChildren } from "react"
 
-import { contentClass, rootClass, titleClass } from "./Banner.css"
+import { contentClass, rootClass } from "./Banner.css"
 
 export interface BannerProps extends PropsWithChildren {
   readonly title: string
 }
 
-export function Banner({ children, title }: BannerProps) {
+export function Banner({ children }: BannerProps) {
   return (
     <div className={rootClass}>
-      {title && <h1 className={titleClass}>{title}:</h1>}
       <div className={contentClass}>{children}</div>
     </div>
   )
