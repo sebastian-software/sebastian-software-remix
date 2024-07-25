0b01edf38c43a2138bee389d838844b92a80faae
Sebastian Werner <s.werner@sebastian-software.de>
2024-07-25 16:33:32 +0200

    feat: responsive layout tweaks

--- app/components/customers/CustomersList.css.ts
+++ app/components/customers/CustomersList.css.ts
@@ -5,25 +5,45 @@ import { elenaWebfont } from "../fonts/elena/Elena.css"
 export const rootClass = style({
   background: "#2A1B28",
   paddingBlock: "6rem",
-  paddingInline: "4vw"
+  paddingInline: "4vw",
+
+  "@media": {
+    "(max-width: 800px)": {
+      paddingInline: "8vw"
+    }
+  }
 })
 
 export const titleClass = style({
   fontFamily: elenaWebfont,
   fontSize: "2rem",
-  color: "white"
+  color: "white",
+  marginBottom: "2rem"
 })
 
 export const logoClass = style({
   width: "100%",
-  maxHeight: "4rem"
+  maxHeight: "4rem",
+
+  "@media": {
+    "(max-width: 800px)": {
+      maxHeight: "2rem"
+    }
+  }
 })
 
 export const listClass = style({
   display: "grid",
-  gridTemplateColumns: "repeat(auto-fit, minmax(150px, 1fr))",
+  gridTemplateColumns: "repeat(auto-fit, minmax(10rem, 1fr))",
   gap: "4rem",
-  alignItems: "center"
+  alignItems: "center",
+
+  "@media": {
+    "(max-width: 800px)": {
+      gridTemplateColumns: "repeat(auto-fit, minmax(7rem, 1fr))",
+      gap: "2rem"
+    }
+  }
 })
 
 export const itemClass = style({
