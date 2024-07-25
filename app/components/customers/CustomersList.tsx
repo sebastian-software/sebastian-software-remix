0ea44e46dd4be20108bde66c5dc17085ed452301
Sebastian Werner <s.werner@sebastian-software.de>
2024-07-25 15:20:28 +0200

    feat: tweaked color, removed log output

--- app/components/customers/CustomersList.tsx
+++ app/components/customers/CustomersList.tsx
@@ -15,8 +15,6 @@ const violetShades = buildShades("#FFFFFF", "#3F2B3D", {
 })
 
 export function CustomersList({ data }) {
-  console.log("Customers:", data)
-
   return (
     <div className={rootClass}>
       <h1 className={titleClass}>Kunden</h1>
@@ -25,7 +23,7 @@ export function CustomersList({ data }) {
           (customer) =>
             hasLogo(customer.logo) && (
               <li key={customer.name} className={itemClass}>
-                <ContentEffect invert boost end="#ded9dd" start="#857784">
+                <ContentEffect invert boost end="#ded9dd" start="#3F2B3D">
                   <CompanyLogo
                     name={customer.logo}
                     alt={customer.name}
