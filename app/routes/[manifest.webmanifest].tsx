7df4f67d2ede9fdcc4e35b56217f4c5d12c0d679
Sebastian Fastner <s.fastner@sebastian-software.de>
2024-07-29 15:12:25 +0200

    feat: Move favicons from public to processed images

---
+++ app/routes/[manifest.webmanifest].tsx
@@ -0,0 +1,22 @@
+// Create webmanifest file with processed icons
+import { webmanifestIcons } from "~/components/favicon/FavIcon"
+
+export const loader = () => {
+  const icons = webmanifestIcons()
+
+  // handle "GET" request
+  // set up our text content that will be returned in the response
+  const webmanifest = JSON.stringify({
+    icons: [
+      { src: icons.icon192, type: "image/png", sizes: "192x192" },
+      { src: icons.icon512, type: "image/png", sizes: "512x512" }
+    ]
+  })
+  // return the text content, a status 200 success response, and set the content type to text/plain
+  return new Response(webmanifest, {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+}
