91f3f04536e7ff1084a3f6e02abf47cc8ccdb163
Sebastian Werner <s.werner@sebastian-software.de>
2024-07-30 15:47:30 +0200

    feat: added new industryIcon component

---
+++ app/components/industry-icon/Industry.css.ts
@@ -0,0 +1,6 @@
+import { style } from "@vanilla-extract/css"
+
+export const rootClass = style({
+  width: "1.5rem",
+  marginBlock: "auto"
+})
---
+++ app/components/industry-icon/Industry.tsx
@@ -0,0 +1,56 @@
+import bankIcon from "./assets/bank.svg"
+import brickIcon from "./assets/brick.svg"
+import carIcon from "./assets/car.svg"
+import ecommerceIcon from "./assets/e-commerce.svg"
+import educationIcon from "./assets/education.svg"
+import gameIcon from "./assets/game.svg"
+import healthIcon from "./assets/health.svg"
+import energyIcon from "./assets/lightbulb.svg"
+import marketingIcon from "./assets/marketing.svg"
+import mobileIcon from "./assets/mobile.svg"
+import networkIcon from "./assets/network.svg"
+import notebookIcon from "./assets/notebook.svg"
+import pillsIcon from "./assets/pills.svg"
+import radioIcon from "./assets/radio.svg"
+import robotIcon from "./assets/robot.svg"
+import shoppingIcon from "./assets/shopping.svg"
+import socialIcon from "./assets/social.svg"
+import trainIcon from "./assets/train.svg"
+import truckIcon from "./assets/truck.svg"
+// Other imports
+import { rootClass } from "./Industry.css"
+
+const industryMap: Record<string, string> = {
+  Automobil: carIcon,
+  Baustoffindustrie: brickIcon,
+  Bildung: educationIcon,
+  "E-Commerce": ecommerceIcon,
+  Energieversorgung: energyIcon,
+  Finanzen: bankIcon,
+  Gesundheitswesen: healthIcon,
+  Handel: shoppingIcon,
+  "Industrie 4.0": robotIcon,
+  Informationstechnologie: networkIcon,
+  "IT-Dienstleistungen": notebookIcon,
+  Marketing: marketingIcon,
+  Nutzfahrzeugindustrie: truckIcon,
+  "Öffentlicher Verkehr": trainIcon,
+  Pharma: pillsIcon,
+  Rundfunk: radioIcon,
+  "Soziale Medien": socialIcon,
+  Spiele: gameIcon,
+  Telekommunikation: mobileIcon
+}
+
+export interface IndustryProps {
+  readonly name: string
+}
+
+export function Industry({ name }: IndustryProps) {
+  const imageSource = industryMap[name]
+  if (imageSource) {
+    return <img className={rootClass} src={imageSource} alt={name} />
+  }
+
+  return null
+}
