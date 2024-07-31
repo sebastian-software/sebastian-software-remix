d2ef350c4ae8ddf99be44b274c140580322b6463
Sebastian Fastner <s.fastner@sebastian-software.de>
2024-07-31 20:38:59 +0200

    feat: use import glob to render post on server side

--- app/routes/blog.$datePart.$urlPart.tsx
+++ app/routes/blog.$datePart.$urlPart.tsx
@@ -1,53 +1,17 @@
-import type { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { useEffect, useState } from "react";
+import { useParams } from "@remix-run/react";
 
-import type { BlogEntryWithContent } from "../blog/blog";
-import { blogList } from "../blog/blog"
-
-export async function loader({ params }: LoaderFunctionArgs) {
-  const data = await blogList()
-
-  const datePart = params.datePart;
-  const urlPart = params.urlPart;
-
-  return data.find((entry) => entry.datePart === datePart && entry.urlPart === urlPart)
-}
-
-function useAsync<T>(asyncFunction: () => Promise<T>): [T | undefined, unknown] {
-  const [value, setValue] = useState<T>();
-  const [error, setError] = useState<unknown>();
-
-  useEffect(() => {
-    asyncFunction().then(setValue).catch(setError);
-  }, [asyncFunction]);
-
-  return [value, error];
-}
-
-function useBlog(datePart: string, urlPart: string) {
-  const [posts, error] = useAsync<BlogEntryWithContent[]>(blogList);
-
-  const post = posts?.find((entry) => entry.longDatePart === datePart && entry.urlPart === urlPart);
-
-  return {
-    error,
-    Component: post?.content
-  }
-}
+import type { Post } from "./blog._index";
 
 export default function BlogEntry() {
-  const data = useLoaderData<typeof loader>()
+  const posts = import.meta.glob<Post>("../blog/*.mdx", {
+    eager: true,
+  });
 
-  const blogEntry = useBlog(data.longDatePart, data.urlPart);
+  const { datePart, urlPart } = useParams();
+  const id = `../blog/${datePart}-${urlPart}.mdx`;
 
-  if (!blogEntry.Component || blogEntry.error) {
-    return <div>Post not found</div>
-  }
+  const post = posts[id]
+  const Component = post.default
 
-  const Component = blogEntry.Component
-
-  return (
-    <div><Component /></div>
-  )
+  return (<div><Component /></div>)
 }
--- app/routes/blog._index.tsx
+++ app/routes/blog._index.tsx
@@ -1,11 +1,40 @@
 import { Link, useLoaderData } from "@remix-run/react"
 
-import { blogList } from "~/blog/blog"
+const SPLIT_DATE_PART = 2
 
-export async function loader() {
-  const data = await blogList()
+export interface Post {
+  default: React.ElementType
+  frontmatter: Record<string, string>
+  summary: string
+}
 
-  return data
+export function loader() {
+  const posts = import.meta.glob<Post>("../blog/*.mdx", {
+    eager: true,
+  });
+
+  console.log(">>>", posts)
+
+  const data = Object.entries(posts).map(([path, { frontmatter, summary }]) => {
+    const filename = path.replace(".mdx", "").split("/").pop()
+
+    if (!filename) {
+      return
+    }
+
+    const parts = filename.split("-")
+    const datePart = parts.slice(0, SPLIT_DATE_PART).join("-")
+    const urlPart = parts.slice(SPLIT_DATE_PART).join("-")
+
+    return ({
+      datePart,
+      urlPart,
+      frontmatter,
+      summary
+    })
+  })
+
+  return data.filter((entry) => entry !== undefined)
 }
 
 export default function BlogIndex() {
