fb3e5ed9d2ce43f94dd3fbd6fcd9f8e27e2a115e
Sebastian Fastner <s.fastner@sebastian-software.de>
2024-07-31 23:19:09 +0200

    feat: add blog layouts

---
+++ app/routes/blog.tsx
@@ -0,0 +1,13 @@
+import { Link, Outlet } from "@remix-run/react";
+
+export default function Blog() {
+  return (
+    <>
+      <div>
+        <Link to="/blog">Übersicht</Link>
+      </div><div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
