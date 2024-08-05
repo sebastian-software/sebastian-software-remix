dfadd955fdb0fb9f2951088f03321de1c023287d
Sebastian Werner <s.werner@sebastian-software.de>
2024-08-05 14:26:19 +0200

    refactor: re-use already used clsx for classname mgr

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -1,4 +1,4 @@
-import classnames from "classnames"
+import { clsx } from "clsx"
 import type { PropsWithChildren } from "react"
 
 import { container, content, footer, root, title } from "./Card.css"
@@ -8,21 +8,21 @@ interface PropsWithStyling extends PropsWithChildren {
 }
 
 export function Card({ children, className }: PropsWithStyling) {
-  return <div className={classnames(root, className)}>{children}</div>
+  return <div className={clsx(root, className)}>{children}</div>
 }
 
 export function CardTitle({ children, className }: PropsWithStyling) {
-  return <div className={classnames(title, className)}>{children}</div>
+  return <div className={clsx(title, className)}>{children}</div>
 }
 
 export function CardContent({ children, className }: PropsWithStyling) {
-  return <div className={classnames(content, className)}>{children}</div>
+  return <div className={clsx(content, className)}>{children}</div>
 }
 
 export function CardFooter({ children, className }: PropsWithStyling) {
-  return <div className={classnames(footer, className)}>{children}</div>
+  return <div className={clsx(footer, className)}>{children}</div>
 }
 
 export function CardContainer({ children, className }: PropsWithStyling) {
-  return <div className={classnames(container, className)}>{children}</div>
+  return <div className={clsx(container, className)}>{children}</div>
 }
